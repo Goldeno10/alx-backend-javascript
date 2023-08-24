cfc0358833f7fccde16240d1fe31e20eae8cbf12
Goldneo10 <ibrahimmuhammad271@gmail.com>
2023-08-24 11:27:43 +0100

    ...

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -8,46 +8,41 @@ const app = http.createServer(async (req, res) => {
       res.statusCode = 200;
       res.end('Hello Holberton School!');
     } else if (req.url === '/students') {
-      try {
-        const db = process.argv[2];
-        if (db) {
-          const database = db.split('.', 2);
-          if (database[1] === 'csv') {
-            const data = await fs.readFile(database, 'utf8');
-            const lines = data.split('\n').filter((line) => line.trim() !== '');
-            const fieldCounters = {};
-            lines.slice(1).forEach((line) => {
-              const fields = line.split(',');
-              if (fields.length >= 4) {
-                const field = fields[3].trim();
+      const db = process.argv[2];
+      if (db) {
+        const database = db.split('.', 2);
+        if (database[1] === 'csv') {
+          const data = await fs.readFile(db, 'utf8');
+          const lines = data.split('\n').filter((line) => line.trim() !== '');
+          const fieldCounters = {};
+          lines.slice(1).forEach((line) => {
+            const fields = line.split(',');
+            if (fields.length >= 4) {
+              const field = fields[3].trim();
 
-                if (!fieldCounters[field]) {
-                  fieldCounters[field] = [];
-                }
-
-                fieldCounters[field].push(fields[0].trim());
+              if (!fieldCounters[field]) {
+                fieldCounters[field] = [];
               }
-            });
 
-            res.setHeader('Content-Type', 'text/plain');
-            res.statusCode = 200;
-
-            res.write('This is the list of our students\n');
-            res.write(`Number of students: ${lines.length - 1}`);
-
-            for (const field in fieldCounters) {
-              if (field) {
-                res.write(`\nNumber of students in ${field}: ${fieldCounters[field].length}. List: ${fieldCounters[field].join(', ')}`);
-              }
+              fieldCounters[field].push(fields[0].trim());
             }
-            res.end();
+          });
+
+          res.setHeader('Content-Type', 'text/plain');
+          res.statusCode = 200;
+
+          res.write('This is the list of our students\n');
+          res.write(`Number of students: ${lines.length - 1}`);
+
+          for (const field in fieldCounters) {
+            if (field) {
+              res.write(`\nNumber of students in ${field}: ${fieldCounters[field].length}. List: ${fieldCounters[field].join(', ')}`);
+            }
           }
+          res.end();
         }
-      } catch (error) {
-        res.setHeader('Content-Type', 'text/plain');
-        res.statusCode = 200;
-        res.end('Hello Holberton School!');
       }
+    
     }
   }
 });
