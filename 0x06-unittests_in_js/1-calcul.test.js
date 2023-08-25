871613d35f11ab3940daf700d4e2ad9fff347f6f
Goldneo10 <ibrahimmuhammad271@gmail.com>
2023-08-25 14:20:39 +0100

    task 2

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul.js');
+
+describe('calculateNumber', () => {
+  it('should correctly sum rounded values', function () {
+    assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+  });
+
+  it('should handle negative values', function () {
+    assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+  });
+
+  it('shout handle zero value', function () {
+    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+  });
+  it('should return Error', function () {
+    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error')
+  })
+
+});
+
