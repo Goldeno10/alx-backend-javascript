a8d43cf767343fa2df6f79a84e8c5899e3a60535
Goldneo10 <ibrahimmuhammad271@gmail.com>
2023-10-02 19:07:29 +0100

    .....

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const fs = require('fs/promises');
+const fs = require('fs').promises;
 
 const app = http.createServer(async (req, res) => {
   if (req.method === 'GET') {
@@ -12,36 +12,57 @@ const app = http.createServer(async (req, res) => {
       if (db) {
         const database = db.split('.', 2);
         if (database[1] === 'csv') {
-          const data = await fs.readFile(db, 'utf8');
-          const lines = data.split('\n').filter((line) => line.trim() !== '');
-          const fieldCounters = {};
-          lines.slice(1).forEach((line) => {
-            const fields = line.split(',');
-            if (fields.length >= 4) {
-              const field = fields[3].trim();
+          try {
+            const data = await fs.readFile(db, 'utf8');
+            const lines = data.split('\n').filter((line) => line.trim() !== '');
+            const fieldCounters = {};
 
-              if (!fieldCounters[field]) {
-                fieldCounters[field] = [];
+            lines.slice(1).forEach((line) => {
+              const fields = line.split(',');
+              if (fields.length >= 4) {
+                const field = fields[3].trim();
+
+                if (!fieldCounters[field]) {
+                  fieldCounters[field] = [];
+                }
+                fieldCounters[field].push(fields[0].trim());
               }
-              fieldCounters[field].push(fields[0].trim());
-            }
-          });
-          res.setHeader('Content-Type', 'text/plain');
-          res.statusCode = 200;
+            });
 
-          res.write('This is the list of our students\n');
-          res.write(`Number of students: ${lines.length - 1}`);
+            res.setHeader('Content-Type', 'text/plain');
+            res.statusCode = 200;
 
-          for (const field in fieldCounters) {
-            if (field) {
-              res.write(`\nNumber of students in ${field}: ${fieldCounters[field].length}. List: ${fieldCounters[field].join(', ')}`);
+            res.write('This is the list of our students\n');
+            res.write(`Number of students: ${lines.length - 1}`);
+
+            for (const field in fieldCounters) {
+              if (Object.prototype.hasOwnProperty.call(fieldCounters, field)) {
+                res.write(`\nNumber of students in ${field}: ${fieldCounters[field].length}. List: ${fieldCounters[field].join(', ')}`);
+              }
             }
+            res.end();
+          } catch (error) {
+            res.statusCode = 500;
+            res.end('Internal Server Error');
           }
-          res.end();
+        } else {
+          res.statusCode = 400;
+          res.end('Bad Request: Invalid file format');
         }
+      } else {
+        res.statusCode = 400;
+        res.end('Bad Request: Missing database argument');
       }
+    } else {
+      res.statusCode = 404;
+      res.end('Not Found');
     }
+  } else {
+    res.statusCode = 405;
+    res.end('Method Not Allowed');
   }
 });
 
-app.listen(1245);
+app.listen(1245, () => {
+  console.log('Server is running on port 1245');
+});
